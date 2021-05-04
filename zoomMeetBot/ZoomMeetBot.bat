9fb0f6c3c915ae6d9103173ceff2840b7765e32a
Sukarna Jana <47217084+Sukarnascience@users.noreply.github.com>
2021-05-04 19:57:16 +0530

    Create ZoomMeetBot.bat

---
+++ zoomMeetBot/ZoomMeetBot.bat
@@ -0,0 +1,9 @@
+echo Hi...
+echo I will load the libraries which required to run this bot
+echo -------------------------------
+echo make sure that you are connected to internet
+echo -------------------------------
+pip3 install pyautogui
+echo -------------------------------
+echo Installation completed Successfully
+echo your robot is ready to run... scheudle the timing by going to 'scheduleTimings.txt' file
