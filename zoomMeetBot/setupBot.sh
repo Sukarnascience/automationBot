dd3527acda09d50b61b0f80ce2e3e9cbb34bda81
Sukarna Jana <47217084+Sukarnascience@users.noreply.github.com>
2021-05-04 17:32:41 +0530

    Update setupBot.sh

--- zoomMeetBot/setupBot.sh
+++ zoomMeetBot/setupBot.sh
@@ -6,8 +6,8 @@ echo " make sure that you are connected to internet"
 echo "-------------------------------"
 sleep 2
 pip3 install pyautogui
+sudo apt-get install scrot
 echo "-------------------------------"
 sleep 1
 echo "Installation completed Successfully"
 echo "your robot is ready to run... scheudle the timing by going to 'scheduleTimings.txt' file"
-
